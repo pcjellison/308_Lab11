28d28be62607356824d889e347c5889449f2cf4c
Payton Jellison <pcjellison@ksu.edu>
2017-11-28 15:01:14 -0600

    Add project files.

---
+++ Lab 11/Lab11.cpp
@@ -0,0 +1,12 @@
+/*
+• Lab11.cpp	(main	function):
+o Create	an	empty	MyString	object
+o Get	user	input	for	MyString	using	overloaded	>>
+o Set	the	first	character	to	'B'	using	overloaded	[]
+o Print	result	using	overloaded	<<
+*/
+
+int main()
+{
+
+}
---
+++ Lab 11/MyString.cpp
@@ -0,0 +1,9 @@
+/*
+• Write	MyString	class	(MyString.h	and	MyString.cpp)	with:
+o Instance	variable:	array	of	100	characters	//Assume	you
+won't	go	past	100	characters*
+
+o Overload	[]	to	access/modify	characters.
+
+o Overload	<<	and	>>
+*/
